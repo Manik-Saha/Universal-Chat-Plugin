ca6ac2c9171d7b787555c0510f2ce54865e884d8
Manik-Saha <maniksaha.00.mailme@gmail.com>
2021-07-05 12:51:09 +0600

    Login

---
+++ Universal_Chat_Plugin/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Universal_Chat_Plugin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin m)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                bool b = db.Admins.CheckAdmin(m);
+                if (b)
+                {
+                    return RedirectToAction("Index", "");
+                }
+                else
+                {
+                    ViewBag.Error = "Your username & password does not match";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Universal_Chat_Plugin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Universal_Chat_Plugin.MvcApplication" Language="C#" %>
